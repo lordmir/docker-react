01f0999e29ebfe5ee7b100ca13f3792f58428b41
lordmir <hase@redfern.xyz>
2022-03-02 18:37:40 +0000

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hase', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hase/i);
   expect(linkElement).toBeInTheDocument();
 });
